9558321ec2ed83c977140adf54779a903de7fc27
Heikki Kontturi <L5339@student.jamk.fi>
2019-02-26 15:13:14 +0200

    -

---
+++ imageRemake/Converter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace imageRemake
+{
+    class Converter
+    {
+        String picturesFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        String filePath;
+        long size;
+
+        public long SaveAs(String fileType, Bitmap bmp, long quality, String nimi)
+        {
+                switch (fileType)
+                {
+                    case ("png"):
+                        filePath = picturesFolder + @"\" + "RMK" + nimi + ".png";
+                        bmp.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    case ("jpeg"):
+                        filePath = picturesFolder + @"\" + "RMK" + nimi + ".jpeg";
+                            // Gets encoder from the jpg image format
+                        ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+                            // Creates an encoder and set it based on the Encoder.Quality parameter category
+                        System.Drawing.Imaging.Encoder mEncoder = System.Drawing.Imaging.Encoder.Quality;
+                            // Creates the EncoderParameters with only one EncoderParameter
+                            // Then gives it our previously created encoder and desired jpeg quality
+                        EncoderParameters mEncoderParams = new EncoderParameters(1);
+                        EncoderParameter mEncoderParam = new EncoderParameter(mEncoder, quality);
+                        mEncoderParams.Param[0] = mEncoderParam;
+                        bmp.Save(filePath ,jpgEncoder,mEncoderParams);
+                        break;
+                    case ("bmp"):
+                        filePath = picturesFolder + @"\" + "RMK" + nimi + ".bmp";
+                        bmp.Save(filePath, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+            }
+            size = new FileInfo(filePath).Length;
+            return size;
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ imageRemake/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace imageRemake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Bitmap img;
+        private long quality = 95;
+        Converter converttori;
+        String newType;
+        String fileName;
+        String picturesFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        long fileSize;
+        long newSize;
+
+        public MainWindow()
+        {
+            converttori = new Converter();
+            InitializeComponent();
+        }
+
+        private void openImg_Click(object sender, RoutedEventArgs e)
+        {
+            rdyNotification.Text = "";
+                // Open a popup with which the user can choose any picture with
+            Microsoft.Win32.OpenFileDialog Opendialog = new Microsoft.Win32.OpenFileDialog();
+            Opendialog.InitialDirectory = picturesFolder;
+            Opendialog.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+            Opendialog.ShowDialog();
+            this.fileName = System.IO.Path.GetFileNameWithoutExtension(Opendialog.FileName);
+                // Checks if the ShowDialog popup got canceled
+            if(Opendialog.FileName != "") {
+                img = new Bitmap(Opendialog.FileName);
+                imgPreview.Source = new BitmapImage(new Uri(Opendialog.FileName));
+                    // Display the image filesize in kilobytes unless it is less than one kilobyte
+                this.fileSize = new System.IO.FileInfo(Opendialog.FileName).Length;
+                if (fileSize > 1024)
+                {
+                    fileSizeDisplay.Text = "File size: " + fileSize / 1024 + " KB";
+                }
+                else
+                {
+                    fileSizeDisplay.Text = "File size: " + fileSize + " bytes";
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                // Save the new image and get its size
+            this.newSize = converttori.SaveAs(newType, img, quality, fileName);
+            rdyNotification.Text = "The image is now saved in " + picturesFolder + " as " + "RMK" + fileName + "." + newType;
+            if (newSize > 1024)
+            {
+                fileSizeDisplay.Text = "New file size: " + newSize / 1024 + " KB";
+            }
+            else
+            {
+                fileSizeDisplay.Text = "New file size: " + newSize + " bytes";
+            }
+            // Updates the preview to the new
+            // (possibly compressed) image
+            imgPreview.Source = new BitmapImage(new Uri(picturesFolder + "\\" + "RMK" + fileName + "." + newType));
+        }
+
+        private void qualitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.quality = (long)qualitySlider.Value;
+        }
+
+        private void pngButton_Checked(object sender, RoutedEventArgs e){this.newType = "png";}
+        private void bmpButton_Checked(object sender, RoutedEventArgs e){this.newType = "bmp";}
+        private void jpegButton_Checked(object sender, RoutedEventArgs e){this.newType = "jpeg";}
+    }
+}
